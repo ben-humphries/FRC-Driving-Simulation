6f80a885147a2347e7daa861e688f90751c7c32b
ben-humphries <ben_humphries@caryacademy.org>
2017-06-06 08:28:18 -0400

    Added top camera view
    position and rotation for top camera view are customizable, button is
    LShift

--- FRC Driving Simulation/Assets/CameraController.cs
+++ FRC Driving Simulation/Assets/CameraController.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
 	public Transform target;
+	Quaternion targetRotation;
+
+	public Vector3 targetRotationEuler;
+	public Vector3 targetPosition;
+	Vector3 startPosition;
+	public float lerpTime;
 
 	public bool followRobot = true;
 
 	// Use this for initialization
 	void Start () {
+
+		targetRotation = Quaternion.Euler (targetRotationEuler);
+
+		startPosition = transform.position;
 		
 	}
 	
@@ -18,5 +28,32 @@ public class CameraController : MonoBehaviour {
 
 		if(followRobot)
 			transform.LookAt (target.position);
+
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			if (followRobot) {
+				StartCoroutine (MoveAndRotateToPoint (transform, transform.position, targetPosition, transform.rotation, targetRotation, lerpTime, false));
+				followRobot = false;
+
+			} else {
+				StartCoroutine(MoveAndRotateToPoint (transform, transform.position, startPosition, transform.rotation, Quaternion.LookRotation ((target.position - startPosition).normalized), lerpTime, true));
+
+			}
+		}
+	}
+	IEnumerator MoveAndRotateToPoint(Transform t, Vector3 startPos, Vector3 finalPos, Quaternion startRot, Quaternion finalRot, float time, bool setFollowRobotTrue){
+		float i = 0f;
+		float rate = 1f / time;
+
+		while (i < 1) {
+
+			i += Time.deltaTime * rate;
+			transform.rotation = Quaternion.Lerp (startRot, finalRot, i);
+			transform.position = Vector3.Lerp (startPos, finalPos, i);
+			yield return null;
+		}
+
+		if (setFollowRobotTrue) {
+			followRobot = true;
+		}
 	}
 }
